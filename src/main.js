575c3a1dfe0e3e007cb92af8027a71ed85970804
SkyeN <skye@dames.es>
2018-10-19 11:30:16 -0700

    finish basic functionalities

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import { DoctorInfo } from "./app-logic";
 
 $(document).ready(function() {
   let searchType = "";
+  $("#clear").click(function() {
+    location.reload(true);
+  });
   $("#options a").click(function() {
     searchType = $(this).attr("id");
     let searchTypeLabel = $(this).text();
@@ -14,37 +17,36 @@ $(document).ready(function() {
   $("#submit").click(function() {
     let searchParameter = $("#searchParameter").val();
     $("#searchParameter").val("");
-
+    $(".result p").val("");
     let newDoctorInfo = new DoctorInfo();
     let promise = newDoctorInfo.getDoctor(searchType, searchParameter);
 
     promise.then(
       function(response) {
         let body = JSON.parse(response);
-        console.log("here");
-        console.log(body.data[0].practices[0].name);
-        for (let doc in body.data) {
-          let path = body.data[doc].practices[0];
+        if (body.data.length === 0) {
+          $("#resultNone").append("No doctor found!");
+          $("#resultAvail").hide();
+        } else {
+          $("#resultAvail").show();
+          for (let doc in body.data) {
+            let path = body.data[doc].practices[0];
 
-          $("#firstName").append(`${body.data[doc].profile.first_name}<br>`);
-          $("#lastName").append(`${body.data[doc].profile.last_name}<br>`);
-          $("#address").append(
-            `Address: ${path.visit_address.street}, ${path.visit_address.city}<br>`
-          );
-          $("#phone").append(`${path.phones[0].number}<br>`);
-          if (!path.website == undefined)
-          {
-          $("#web").append(`${path.website}<br>`);
+            $("#firstName").append(`${body.data[doc].profile.first_name}<br>`);
+            $("#lastName").append(`${body.data[doc].profile.last_name}<br>`);
+            $("#address").append(
+              `${path.visit_address.street}, ${path.visit_address.city}<br>`
+            );
+            $("#phone").append(`${path.phones[0].number}<br>`);
+            if (!path.website == undefined) {
+              $("#web").append(`${path.website}<br>`);
+            } else {
+              $("#web").append(`N/A<br>`);
+            }
+            let acceptingStatus;
+            acceptingStatus = path.accepts_new_patients === true ? "Yes" : "No";
+            $("#accepting").append(`${acceptingStatus}<br>`);
           }
-          else
-          {
-            $("#web").append(`N/A<br>`);
-          }
-          let acceptingStatus;
-          acceptingStatus = path.accepts_new_patients === true ? "Yes" : "No";
-          $("#accepting").append(
-            `${acceptingStatus}<br>`
-          );
         }
       },
       function(error) {
