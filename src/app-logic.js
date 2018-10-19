e5740df654300843ada977095b08501b70a7ba9d
SkyeN <skye@dames.es>
2018-10-19 11:10:48 -0700

    fix call url

--- src/app-logic.js
+++ src/app-logic.js
@@ -1,21 +1,21 @@
 export class DoctorInfo
 {
-    getDoctor(searchType,searchParameter)
-    {
-        return new Promise(function(resolve,reject){
-            let request = new XMLHttpRequest();
-            let url = `https://api.betterdoctor.com/2016-03-01/doctors?${searchType}=${searchParameter}location=wa-seattle&user_key=process.env.exports.apiKey`
-            request.onload = function() {
-                if(this.status === 200)
-                {
-                    resolve(request.response);
-                }
-                else{
-                    reject(Error(request.statusText));
-                }
-            };
-            request.open("GET",url,true);
-            request.send();
-        });
-    }
+  getDoctor(searchType,searchParameter)
+  {
+    return new Promise(function(resolve,reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?${searchType}=${searchParameter}&location=wa-seattle&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if(this.status === 200)
+        {
+          resolve(request.response);
+        }
+        else{
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET",url,true);
+      request.send();
+    });
+  }
 }
